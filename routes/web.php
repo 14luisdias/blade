1ffd4c4afe35dba07cfd290a971143855dc24bf5
Luis Antonio <14luisdias@gmail.com>
2025-09-18 13:10:08 -0500

    Código PHP e Comentários

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/passagem/dados', function (){
+    return view('exemplos.passagem_dados', [
+        'nome'=> 'TreinaWb',
+        'descricao' => 'Escola de Desenvolvimento'
+    ]);
+    
 });
